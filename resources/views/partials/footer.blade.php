1b5b322a106b5fcaecb5ff46973de0d414c168a3
Andrew <andrew@delisle.se>
2021-04-13 13:40:00 +0200

    Fixing footer and menus

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,27 +1,38 @@
 <footer class="content-info text-white pt-4 pb-4">
 	<div class="container">
 		<div class="row">
-			<nav class="nav-footer col-md-4">
-				<h3 class="text-red">Våra Hallar</h3>
+			<nav class="nav-footer col-md-4 mb-4 mb-md-0">
+				<h3 class="text-red"><strong>Våra Hallar</strong></h3>
 				@if (has_nav_menu('footer_navigation'))
 					{!! wp_nav_menu(['theme_location' => 'footer_navigation', 'menu_class' => 'nav flex-column']) !!}
         		@endif
 			</nav>
-			<div class="col-md-4 mt-2 mt-md-0">
-				<h3>Kontakt</h3>
-				@php dynamic_sidebar('sidebar-footer') @endphp
-			</div>
-			<div class="col-md-4">
+			<div class="col-md-3 mt-2 mt-md-0 mb-4 mb-md-0">
+				<h3><strong>Kontakt</strong></h3>
 				<div class="row">
-					<div class="col-6">
+					<div class="col-2 col-md-3">
+						<img src="@asset('images/icons/pin.png')" class="img-fluid" alt="Pin" />
+					</div>
+					<div class="col-10 col-md-9">
+						<p>Padel United AB<br />
+						Västeråsgatan 5<br />
+						113 43 Stockholm
+					</div>
+				</div>
+			</div>
+			<div class="col-md-5 mb-4 mb-md-0">
+				<div class="row">
+					<div class="col-4">
 						<img class="img-fluid" src="@asset('images/footer-logo.png')" alt="Padel United Footer Logga" />
 					</div>
-					<div class="col-6">
-					@if (has_nav_menu('footer_second_navigation'))
-						{!! wp_nav_menu(['theme_location' => 'footer_second_navigation', 'menu_class' => 'nav flex-column']) !!}
-        			@endif
-					<br />
-					<p>&copy; {{ date('Y') }} {{ get_bloginfo('name', 'display') }}</p>
+					<div class="col-8">
+						@if (has_nav_menu('footer_second_navigation'))
+							{!! wp_nav_menu(['theme_location' => 'footer_second_navigation', 'menu_class' => 'nav flex-column']) !!}
+						@endif
+						<br />
+						<div>
+							<p>&copy; {{ date('Y') }} Padel United AB</p>
+						</div>
 					</div>
 				</div>
 			</div>
