b54ac668a2a25663e790e904f3690870a9a45998
Andrew <andrew@delisle.se>
2021-04-16 10:05:55 +0200

    Trying to fix slider direction

--- resources/views/partials/content-single-omrade_hall.blade.php
+++ resources/views/partials/content-single-omrade_hall.blade.php
@@ -2,8 +2,6 @@
   <div class="entry-content">
     {!! do_shortcode( '[visa_general_info_hallar]') !!}
 
-    {!! do_shortcode( '[nyheter_med_etikett etiketter="flemingsberg" limit="3"]' ) !!}
-
     @php the_content() @endphp
   </div>
 </article>
