5ea56afb558bc533a20c19bc65fab7329a39a326
Andrew <andrew@delisle.se>
2021-04-20 15:02:45 +0200

    Adding hallar on områdes pages

--- resources/views/partials/content-single-omrade_hall.blade.php
+++ resources/views/partials/content-single-omrade_hall.blade.php
@@ -1,6 +1,6 @@
 <article @php post_class() @endphp>
   <div class="entry-content">
-    @if (get_post_meta(get_the_ID(), 'hall_sida', true)))
+    @if (get_post_meta(get_the_ID(), 'hall_sida', true))
       {!! do_shortcode( '[visa_general_info_hallar]') !!}
     @endif
 
