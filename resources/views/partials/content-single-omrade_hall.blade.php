a53f7981c900d2cc8b61ea0405a7371c4d25ea6d
Andrew <andrew@delisle.se>
2021-04-22 14:50:04 +0200

    Fixing with hall list on områdes sida

--- resources/views/partials/content-single-omrade_hall.blade.php
+++ resources/views/partials/content-single-omrade_hall.blade.php
@@ -8,38 +8,13 @@
     <div class="container alignfull pu-darkblue-bg pt-4 pb-4">
       <h2 class="pt-4 pb-4 text-white text-center text-uppercase"><strong>VÅRA HALLAR I {!! the_title() !!}</strong></h2>
       <div class="container ps-0 pe-0">
-        <div class="scrolling-wrapper row flex-row flex-nowrap ps-0 pe-0 pe-md-4">
-          @php $counter = 1; @endphp
+        <div class="row">
           @while ($get_children_pages->have_posts())
-            @php $get_children_pages->the_post() @endphp
-            <div class="col-md-4 col-10 pe-md-1 mb-4">
-                <div class="@php if ($counter % 3 == 1) {
-                        echo 'pu-warmyellow-bg';
-                    } else if ($counter % 3 == 2) {
-                        echo 'pu-yellow-bg';
-                    } else if ($counter % 3 == 0) {
-                        echo 'pu-orange-bg';
-                    } @endphp pt-2 pb-2"></div>
-                <div class="@php if ($counter % 3 == 1) {
-                        echo 'pu-warmyellow-bg';
-                    } else if ($counter % 3 == 2) {
-                        echo 'pu-yellow-bg';
-                    } else if ($counter % 3 == 0) {
-                        echo 'pu-orange-bg';
-                    } @endphp mt-2 p-4 clearfix text-black">
-                    <div class="container d-flex h-100 p-0">
-                      <div class="row">
-                          <div class="col-12 justify-content-center align-self-center">
-                              <h4><?php the_title() ?></h4>
-                          </div>
-                      </div>
-                    </div>
-                    <p><?= get_the_excerpt() ?></p>
-                    <a href="<?php echo(get_the_permalink()) ?>" class="btn btn-primary float-end">Gå till hallen <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
+          @php $get_children_pages->the_post() @endphp
+            <div class="col text-center mb-4">
+              <a href="<?php echo(get_the_permalink()) ?>" class="btn btn-large btn-huge btn-primary text-uppercase w-280 no-hover"><?php the_title() ?> <i class="fas fa-arrow-circle-right"></i></a>
             </div>
-            @php $counter++; @endphp
-          @endwhile
+            @endwhile
           @php wp_reset_postdata() @endphp
         </div>
       </div>
