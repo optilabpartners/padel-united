a3c01db9226705f27a7528ae322111e2be96e84a
Andrew <andrew@delisle.se>
2021-04-28 14:33:14 +0200

    Fixing with footer layout

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -27,7 +27,7 @@
 
 @if (('omrade_hall' == get_post_type() && get_post_meta(get_the_ID(), 'hall_sida', true)) || is_front_page())
 <div class="jumbotron d-flex align-items-center" style="box-shadow: inset 0 0 0 1000px rgba(0,0,0,.6); background-image:url('{!! $featured_image !!}'); background-size: cover; background-repeat: no-repeat; background-position: top center;">
-	<div class="container justify-content-center text-white pt-4 pb-4">
+	<div class="container justify-content-center text-white pb-4">
 		<div class="row text-center">
 			<div class="col-12">
 			@if ('omrade_hall' == get_post_type() && get_post_meta(get_the_ID(), 'hall_sida', true))
@@ -44,7 +44,7 @@
 	</div>
 </div>
 @endif
-<footer class="content-info text-white pb-4">
+<footer class="content-info text-white pt-4 pb-4">
 	<div class="container">
 		<div class="row">
 			<nav class="nav-footer col-md-4 mb-4 mb-md-0">
@@ -54,16 +54,13 @@
         		@endif
 			</nav>
 			<div class="col-md-3 mt-2 mt-md-0 mb-4 mb-md-0">
-				<h3>Kontakt</h3>
+				<h3>Länkar</h3>
 				<div class="row">
-					<div class="col-2 col-md-3">
-						<img src="@asset('images/icons/pin.png')" class="img-fluid" alt="Pin" />
-					</div>
-					<div class="col-10 col-md-9">
-						<p>Padel United AB<br />
-						Västeråsgatan 5<br />
-						113 43 Stockholm
-					</div>
+					@if (has_nav_menu('footer_second_navigation'))
+						{!! wp_nav_menu(['theme_location' => 'footer_second_navigation', 'menu_class' => 'nav flex-column']) !!}
+					@endif
+					
+					
 				</div>
 			</div>
 			<div class="col-md-5 mb-4 mb-md-0">
@@ -72,13 +69,10 @@
 						<img class="img-fluid" src="@asset('images/footer-logo.png')" alt="Padel United Footer Logga" />
 					</div>
 					<div class="col-9">
-						@if (has_nav_menu('footer_second_navigation'))
-							{!! wp_nav_menu(['theme_location' => 'footer_second_navigation', 'menu_class' => 'nav flex-column']) !!}
-						@endif
-						<br />
-						<div>
-							<p>&copy; {{ date('Y') }} Padel United AB</p>
-						</div>
+						<p>Padel United AB<br />
+						Västeråsgatan 5<br />
+						113 43 Stockholm
+						<p>&copy; {{ date('Y') }} Padel United AB</p>
 					</div>
 				</div>
 			</div>
