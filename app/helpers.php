6dac613c77f78c98a4fdea9d693d01b5be9c83ba
Andrew <andrew@delisle.se>
2021-04-29 12:48:21 +0200

    Fixes all over the places with padding etc etc

--- app/helpers.php
+++ app/helpers.php
@@ -248,13 +248,13 @@ function visa_generella_ikoner( $atts ) {
     $atts = shortcode_atts( array(), $atts, 'visa_generella_ikoner' );
     ob_start();
     ?>
-    <div class="container alignfull pu-darkblue-bg pb-4">
-        <div class="text-center text-white">
+    <div class="container alignfull pu-darkblue-bg pt-4 pb-4">
+        <div class="text-center text-white pt-4 pb-4">
             <h2 class="pt-4 pb-4 fw-bold">Vårt hjärta slår för spelet</h2>
         </div>
         <div class="container text-white ps-0 pe-0 ps-md-3 pe-md-3 text-center">
             <div class="row">
-            <p>Padel är en fantastisk sport som alla kan utöva – oavsett erfarenhet eller spelnivå. På Padel United erbjuder vi spel för såväl nybörjare som elitspelare, för barn, ungdomar, vuxna och pensionärer och varje padelhall erbjuder också banor för handikappade där man kan komma in med rullstol. Välkommen till Padel United! - vi kan garantera ett personligt bemötande och en rolig stund på banan.</p>
+                <p>Padel är en fantastisk sport som alla kan utöva – oavsett erfarenhet eller spelnivå. På Padel United erbjuder vi spel för såväl nybörjare som elitspelare, för barn, ungdomar, vuxna och pensionärer och varje padelhall erbjuder också banor för handikappade där man kan komma in med rullstol. Välkommen till Padel United! - vi kan garantera ett personligt bemötande och en rolig stund på banan.</p>
             </div>
         </div>
         <div class="container ps-0 pe-0 ps-md-3 pe-md-3 text-white text-center w-75">
